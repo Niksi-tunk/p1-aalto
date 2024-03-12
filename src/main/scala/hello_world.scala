dbc3999134cce831691de8a6cef5d3bfa17722a9
Luukas Pörtfors <lajp@iki.fi>
2024-03-12 20:23:25 +0200

    feat: make p2 course

--- src/main/scala/hello_world.scala
+++ src/main/scala/hello_world.scala
@@ -1,5 +1,23 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+// Below is a main function written in Scala 3
+// However, the function has a problems
+// that cause compilation to fail
+//
+// Find and fix the problems
+// and make the program runnable
+//
+// The program should print out the
+// `msg` variable with the contents: "Hello world!"
 
-def msg = "I was compiled by Scala 3. :)"
+@main
+def hello(): Unit =
+  // Create message
+  val msg = ""
+
+  // Add "Hello" to message
+  msg += "Hello"
+  // Add rest of message
+  msg += " world!"
+
+  // Print out message
+  println(msg
+
